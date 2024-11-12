18300202529f9a7664951d1885be691c7c193511
Ondra Pelech <ondra.pelech@gmail.com>
2024-11-12 22:21:08 +0100

    Liquibase loggs in AnalyticsArgs as INFO (instead of ERROR)

--- liquibase-core/src/main/java/com/github/sideeffffect/liquibase/core/CoreLiquibase.java
+++ liquibase-core/src/main/java/com/github/sideeffffect/liquibase/core/CoreLiquibase.java
@@ -14,6 +14,7 @@ import liquibase.ui.LoggerUIService;
 import java.sql.Connection;
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Level;
 
 public class CoreLiquibase {
     public static void runMigration(Connection connection, String changeLogFile) throws Exception {
@@ -24,7 +25,8 @@ public class CoreLiquibase {
         updateCommand.addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.LOG);
         Scope.enter(Map.of(
                 Scope.Attr.ui.name(), new LoggerUIService(), // https://github.com/liquibase/liquibase/issues/2396
-                AnalyticsArgs.ENABLED.getKey(), Boolean.FALSE // https://github.com/liquibase/liquibase/pull/6412
+                AnalyticsArgs.ENABLED.getKey(), Boolean.FALSE, // https://github.com/liquibase/liquibase/pull/6412
+                AnalyticsArgs.LOG_LEVEL.getKey(), Level.INFO // https://github.com/liquibase/liquibase/pull/6510
         ));
         var result = updateCommand.execute().getResults();
         var statusCode = Optional.ofNullable(result.get("statusCode"));
