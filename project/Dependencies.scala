d0e0a41efdaa9ed64423345219a5b969d644063b
Ondra Pelech <ondra.pelech@gmail.com>
2025-11-23 00:51:32 +0100

    doobie 1.0.0-RC11, zio-config 4.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val doobie = "1.0.0-RC10"
+    val doobie = "1.0.0-RC11"
     val liquibase = "4.33.0"
     val pureconfig = "0.17.9"
     val zioCats = "23.1.0.10"
-    val zioConfig = "4.0.4"
+    val zioConfig = "4.0.5"
   }
 
   val doobie = "org.tpolecat" %% "doobie-hikari" % Versions.doobie
